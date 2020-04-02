05a4f0e454fd235613a113b65c51ebdfae72caa0
Dylan White <dylanhwite@Gmail.com>
2020-04-02 15:52:58 +1030

    Added Vector3 Class Details

---
+++ PointsAndVectors/PointsAndVectors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PointsAndVectors
+{
+    class Program
+    {
+        class Vector2
+        {
+            public float x, y;
+        }
+
+        class Vector3
+        {
+            public float x, y, z;
+            
+            public Vector3(float x, float y, float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+            {
+                return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+            }
+            public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+            {
+                return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+            }
+
+        }
+    }
+}
