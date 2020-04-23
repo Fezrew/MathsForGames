4044378676118fd90c7541356e90a15d76aa5ee9
Dylan White <dylanhwite@Gmail.com>
2020-04-23 13:20:01 +0930

    Dot and Cross added

--- PointsAndVectors/PointsAndVectors/Program.cs
+++ PointsAndVectors/PointsAndVectors/Program.cs
@@ -7,6 +7,22 @@ namespace PointsAndVectors
         class Vector2
         {
             public float x, y;
+
+            public Vector2(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            Vector2 GetPerpendicularRH()
+            {
+                return new Vector2( -y, x );
+            }
+
+            Vector2 GetPerpendicularLH()
+            {
+                return new Vector2( y, -x );
+            }
         }
 
         class Vector3
@@ -81,21 +97,70 @@ namespace PointsAndVectors
             {
                 return (this / Magnitude());
             }
+            #endregion
+
+            #region Dot&Cross
+            public float Dot(Vector3 rhs)
+            {
+                return x * rhs.x + y * rhs.y + z * rhs.z;
+            }
+
+            public Vector3 Cross(Vector3 rhs)
+            {
+                return new Vector3(
+               y * rhs.z - z * rhs.y,
+               z * rhs.x - x * rhs.z,
+               x * rhs.y - y * rhs.x);
+            }
+
+            float AngleBetween(Vector3 other)
+            {
+                // normalise the vectors
+                Vector3 a = GetNormalised();
+                Vector3 b = other.GetNormalised();
+
+                // calculate the dot product
+                float d = a.x * b.x + a.y * b.y + a.z * b.z;
+
+                // return the angle between them
+                return (float)Math.Acos(d);
+            }
+
+            #endregion
 
             static void Main()
             {
-                Vector3 Vec1 = new Vector3(0, 1, 2);
-                Vector3 Vec2 = new Vector3(9, -2, 7);
-                float mag = Vec1.Magnitude();
-                float dist = Vec1.Distance(Vec2);
+                Vector3 Vec1 = new Vector3(0.857f, 0, -0.514f);
+                Vector3 Vec2 = new Vector3(-7.5f, 0, 7);
+                Vector3 Vec3 = new Vector3(10, 0, 18);
+                Vector3 Upward = new Vector3(0, 1, 0);
 
-                Console.WriteLine($"{dist}");
-                Console.ReadKey();
+                Vector3 Forward = Vec1 - Vec2;
+                Vector3 Right = Forward.Cross(Upward);
+                Console.WriteLine($"Forward Vector: {Forward.x}, {Forward.y}, {Forward.z}" +
+                    $"\nRightHand Vector: {Right.x}, {Right.y}, {Right.z}");
+                Vector3 EnemyToPlayer = Vec3 - Vec2;
+                float IsRight = Right.Dot(EnemyToPlayer);
+                if(IsRight >= 0)
+                {
+                    Console.WriteLine("\nYou are to the right of the enemy");
+                }
+                else
+                {
+                    Console.WriteLine("\nYou are not to the right of the enemy");
+                }
+
+                //Magnitude&Distance
+
+                //float mag = Vec1.Magnitude();
+                //float dist = Vec1.Distance(Vec2);
+                //Console.WriteLine($"{dist}");
 
                 //Normalisation Code
+
                 //myVec /= mag;
+                Console.ReadKey();
             }
-            #endregion
 
             #region Exercise Answers
             //Question 2 
@@ -108,9 +173,10 @@ namespace PointsAndVectors
             // Distance between (-2, 5.5) and (9, -22) is 29.618406
             // Distance between (0, 1, 2) and (9, -2, 7) is 10.723805
             //
-            //
-            //
-            //
+            // Question 4
+            // 
+            // You are to the right of the enemy
+            // 
             //
             #endregion
         }
