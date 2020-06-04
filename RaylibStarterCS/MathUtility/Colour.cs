c5078bace5fe33d49cea38d42534df044da1230c
Dylan White <dylanhwite@Gmail.com>
2020-06-04 10:59:57 +0930

    Added template class for colour

---
+++ RaylibStarterCS/MathUtility/Colour.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtility
+{
+    public class Colour
+    {
+        public uint colour;
+
+        public Colour()
+        {
+
+        }
+
+        public Colour(byte r, byte g, byte b, byte a)
+        {
+
+        }
+
+        public byte GetRed()
+        {
+            throw new NotImplementedException();
+        }
+        public byte GetGreen()
+        {
+            throw new NotImplementedException();
+        }
+        public byte GetBlue()
+        {
+            throw new NotImplementedException();
+        }
+        public byte GetAlpha()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRed(byte r)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetGreen(byte g)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetBlue(byte b)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetAlpha(byte a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
